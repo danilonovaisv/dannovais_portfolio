64a734f51e877fdfee3da108fec5e85c9fe9665d
danilonovaisv <danilo_novais@yahoo.com.br>
2025-12-02 06:12:39 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import App from "../App";
+// app/page.tsx
+import Hero from '../components/hero/Hero';
+import Manifesto from '../components/hero/Manifesto';
+// quando tiver as outras seções, importar aqui também
 
-export default function Home() {
-  return <App />;
+export default function HomePage() {
+  return (
+    <main className="min-h-screen">
+      <Hero />
+      <Manifesto />
+      {/* TODO: PortfolioShowcase, FeaturedProjects, Clients, Contact, Footer */}
+    </main>
+  );
 }
